7056e2e8c63e50dafbd0a08b1f95b88449ebe3b9
Simao Belchior <simaobelchior@gmail.com>
2016-07-31 18:32:12 +0100

    Add language switcher. changed default locale to pt

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module Marianaesimao
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.enforce_available_locales = false
+    config.i18n.default_locale = :pt
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  #scope "(/:locale)", locale: /en|pt/ do
+  #end
+
+  get "/:locale" => "home#index"
+
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
