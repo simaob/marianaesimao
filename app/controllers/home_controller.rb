01136c9dd45faeab953a7d59ef0c8badda4070cc
Simao Belchior <simaobelchior@gmail.com>
2016-08-07 13:00:09 +0100

    Adds answer to RSVP. tries to style it

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
   end
 
   def rsvp
-    RsvpMailer.rsvp(params[:name], params[:email], params[:notes]).
+    RsvpMailer.rsvp(params[:name], params[:email], params[:notes], params[:answer]).
       deliver_later
     redirect_to root_path, notice: "Email sent!"
   end
--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -1,10 +1,11 @@
 class RsvpMailer < ApplicationMailer
   default from: "no-reply@marianaesimao.com", to: "simaoemariana@gmail.com"
 
-  def rsvp name, email, notes
+  def rsvp name, email, notes, answer
     @name = name
     @email = email
     @notes = notes
-    mail(subject: "Someone RSVPed!")
+    @answer = answer
+    mail(subject: "[RSVP] [#{@name} - #{@answer}")
   end
 end
